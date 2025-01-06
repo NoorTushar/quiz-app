840ad32f062214b30ce168c6f5751eeb70e6b8c1
NoorTushar <tushar.noor.khan@gmail.com>
2025-01-06 19:27:45 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ModeToggle } from "./components/mode-toggle";
+function App() {
+   return (
+      <>
+         <div className="min-h-screen">
+            <ModeToggle />
+         </div>
+      </>
+   );
+}
+
+export default App;
